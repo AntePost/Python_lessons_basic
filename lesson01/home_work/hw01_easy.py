453c5d23ca71bde5529f0c0ac2d3421379bd3f4e
AntePost <is1213791@gmail.com>
2018-09-29 21:32:16 +0300

    Added homework solutions for lesson 1.

--- lesson01/home_work/hw01_easy.py
+++ lesson01/home_work/hw01_easy.py
@@ -1,5 +1,5 @@
 
-__author__ = 'Ваши Ф.И.О.'
+__author__ = 'Иван Кузнецов'
 
 # Задача-1: Дано произвольное целое число (число заранее неизвестно).
 # Вывести поочередно цифры исходного числа (порядок вывода цифр неважен).
@@ -9,16 +9,78 @@ __author__ = 'Ваши Ф.И.О.'
 
 # код пишем тут...
 
+# Easy, task 1, first version
+# Через цикл for
+num = 58375
+num_str = str(num)
+print("Digits in number %s are:" % num_str)
+for char in num_str:
+    print(char)
+
+# Easy, task 1, second version
+# Через цикл while
+num = 58375
+num_str = str(num)
+print("Digits in number %s are:" % num_str)
+i = 0
+while i < len(num_str):
+    print(num_str[i])
+    i += 1
+
+# Easy, task 1, third version
+# Через цикл while и арифметические операции
+num = 58375
+num_str = str(num)
+print("Digits in number %s are:" % num_str)
+i = 1
+while i <= len(num_str):
+    print(num % 10)
+    num = num // 10
+    i += 1
+
+# Easy, task 1, fourth version
+# Через цикл while и арифметические операции без использования len()
+num = 58375
+print("Digits in number %d are:" % num)
+while num != 0:
+    print(num % 10)
+    num = num // 10
 
 # Задача-2: Исходные значения двух переменных запросить у пользователя.
 # Поменять значения переменных местами. Вывести новые значения на экран.
 # Подсказка:
-# * постарайтесь сделать решение через дополнительную переменную 
+# * постарайтесь сделать решение через дополнительную переменную
 #   или через арифметические действия
 # Не нужно решать задачу так:
 # print("a = ", b, "b = ", a) - это неправильное решение!
 
+# Easy, task 2, first version
+# Через третью переменную
+var_a = input("Enter a variable: ")
+var_b = input("Enter another variable: ")
+var_c = var_a
+var_a = var_b
+var_b = var_c
+print("First variable is now:", var_a)
+print("Second variable is now:", var_b)
+
+# Easy, task 2, second version
+# Через арифметические операции
+var_a = float(input("Enter a number: "))
+var_b = float(input("Enter another number: "))
+var_a = var_a + var_b
+var_b = var_a - var_b
+var_a = var_a - var_b
+print("First number is now:", var_a)
+print("Second number is now:", var_b)
 
 # Задача-3: Запросите у пользователя его возраст.
 # Если ему есть 18 лет, выведите: "Доступ разрешен",
 # иначе "Извините, пользование данным ресурсом только с 18 лет"
+
+# Easy, task 3
+age = int(input("Введите Ваш возраст: "))
+if age >= 18:
+    print("Доступ разрешен")
+else:
+    print("Извините, пользование данным ресурсом только с 18 лет")
--- lesson01/home_work/hw01_normal.py
+++ lesson01/home_work/hw01_normal.py
@@ -10,6 +10,30 @@ __author__ = 'Ваши Ф.И.О.'
 # * постарайтесь решить задачу с применением арифметики и цикла while;
 # * при желании и понимании решите задачу с применением цикла for.
 
+# Normal, task 1, first version
+# С циклом for и len()
+num = 58375
+num_str = str(num)
+i = 0
+j = 0
+while i < len(num_str):
+    for char in num_str:
+        if j < int(char):
+            j = int(char)
+    i += 1
+print("Number is %s" % num_str)
+print("The biggest digit in this number is", j)
+
+# Normal, task 1, second version
+# Без цикла for и len()
+num = 58375
+print("Number is %d" % num)
+j = 0
+while num != 0:
+    if j < num % 10:
+        j = num % 10
+    num = num // 10
+print("The biggest digit in this number is", j)
 
 # Задача-2: Исходные значения двух переменных запросить у пользователя.
 # Поменять значения переменных местами. Вывести новые значения на экран.
@@ -18,6 +42,13 @@ __author__ = 'Ваши Ф.И.О.'
 # * постарайтесь сделать решение через действия над числами;
 # * при желании и понимании воспользуйтесь синтаксисом кортежей Python.
 
+# Normal, task 2
+# Через кортежи, а через арифметику решено в Easy-задании
+var_a = input("Enter a variable: ")
+var_b = input("Enter another variable: ")
+var_a, var_b = var_b, var_a
+print("First variable is now:", var_a)
+print("Second variable is now:", var_b)
 
 # Задача-3: Напишите программу, вычисляющую корни квадратного уравнения вида
 # ax² + bx + c = 0.
@@ -25,3 +56,25 @@ __author__ = 'Ваши Ф.И.О.'
 # Для вычисления квадратного корня воспользуйтесь функцией sqrt() модуля math:
 # import math
 # math.sqrt(4) - вычисляет корень числа 4
+
+# Normal, task 3
+import math
+
+
+coef_a = float(input("Enter coefficient a: "))
+coef_b = float(input("Enter coefficient b: "))
+coef_c = float(input("Enter coefficient c: "))
+D = coef_b ** 2 - 4 * coef_a * coef_c
+if D < 0:
+    print("Discriminant is:", D)
+    print("Your equation has complex roots. :(")
+elif D == 0:
+    x = -coef_b / (2 * coef_a)
+    print("Discriminant is:", D)
+    print("The only root is", x)
+else:
+    x_1 = (-coef_b - math.sqrt(D)) / (2 * coef_a)
+    x_2 = (-coef_b + math.sqrt(D)) / (2 * coef_a)
+    print("Discriminant is:", D)
+    print("First root is:", x_1)
+    print("Second root is:", x_2)
