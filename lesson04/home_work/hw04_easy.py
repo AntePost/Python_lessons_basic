e1e79132debb61fda4b1b3d31c38246083f4123b
AntePost <is1213791@gmail.com>
2018-10-09 16:26:16 +0300

    Added homework solutions for Easy and Normal (except for Normal, task 2, version 2) and Hard, task 1

--- lesson04/home_work/hw04_easy.py
+++ lesson04/home_work/hw04_easy.py
@@ -1,14 +1,27 @@
 # Все задачи текущего блока решите с помощью генераторов списков!
 
 # Задание-1:
-# Дан список, заполненный произвольными целыми числами. 
+# Дан список, заполненный произвольными целыми числами.
 # Получить новый список, элементы которого будут
 # квадратами элементов исходного списка
 # [1, 2, 4, 0] --> [1, 4, 16, 0]
 
+# Easy, task 1
+orig_list = [1, 2, 4, 0]
+sqr_list = [item ** 2 for item in orig_list]
+print(sqr_list)
+
 # Задание-2:
 # Даны два списка фруктов.
 # Получить список фруктов, присутствующих в обоих исходных списках.
+
+# Easy, task 2
+fr_list_1 = ["apple", "banana", "grapefruit", "mango", "melon", "orange",
+             "watermelon"]
+fr_list_2 = ["apple", "mango", "watermelon"]
+
+fr_list_3 = [item for item in fr_list_1 if item in fr_list_2]
+print(fr_list_3)
 
 # Задание-3:
 # Дан список, заполненный произвольными числами.
@@ -16,3 +29,13 @@
 # + Элемент кратен 3
 # + Элемент положительный
 # + Элемент не кратен 4
+
+# Easy, task 3
+import random
+
+
+num_list = [random.randint(-10, 40) for _ in range(20)]
+num_list_2 = [item for item in num_list if (item > 0) and (item % 3 == 0) and
+              (item % 4 != 0)]
+print(num_list)
+print(num_list_2)
--- lesson04/home_work/hw04_normal.py
+++ lesson04/home_work/hw04_normal.py
@@ -4,6 +4,13 @@
 # Т.е. из строки "mtMmEZUOmcq" нужно получить ['mt', 'm', 'mcq']
 # Решить задачу двумя способами: с помощью re и без.
 
+# Normal, task 1, version 1
+# Через re
+# Задание понял в том смысле, чтобы вывести все блоки символов в нижнем регистре
+# Решение подсмотрел в интернете и на чужом форке
+import re
+
+
 line = 'mtMmEZUOmcqWiryMQhhTxqKdSTKCYEJlEZCsGAMkgAYEOmHBSQsSUHKvSfbmxULaysmNO'\
        'GIPHpEMujalpPLNzRWXfwHQqwksrFeipEUlTLeclMwAoktKlfUBJHPsnawvjPhfgewVzK'\
        'TUfSYtBydXaVIpxWjNKgXANvIoumesCSSvjEGRJosUfuhRRDUuTQwLlJJJDdkVjfSAHqn'\
@@ -20,14 +27,36 @@ line = 'mtMmEZUOmcqWiryMQhhTxqKdSTKCYEJlEZCsGAMkgAYEOmHBSQsSUHKvSfbmxULaysmNO'\
        'XiUWgsKQrDOeZoNlZNRvHnLgCmysUeKnVJXPFIzvdDyleXylnKBfLCjLHntltignbQoiQ'\
        'zTYwZAiRwycdlHfyHNGmkNqSwXUrxGc'
 
+match = re.findall("[a-z]+", line)
+print(match)
+
+# Normal, task 1, version 2
+# Без re
+# Решение подсмотрел на чужом форке, сделал по аналогии
+char_list = list(line)
+char_list_low = []
+for item in char_list:
+    if item.isupper():
+        char_list_low.append('_')
+    else:
+        char_list_low.append(item)
+line_new = ''.join(char_list_low)
+char_list_low = line_new.split('_')
+char_list_low = [item for item in char_list_low if item != '']
+print(char_list_low)
 
 # Задание-2:
 # Вывести символы в верхнем регистре, слева от которых находятся
 # два символа в нижнем регистре, а справа - два символа в верхнем регистре.
-# Т.е. из строки 
+# Т.е. из строки
 # "GAMkgAYEOmHBSQsSUHKvSfbmxULaysmNOGIPHpEMujalpPLNzRWXfwHQqwksrFeipEUlTLec"
 # нужно получить список строк: ['AY', 'NOGI', 'P']
 # Решить задачу двумя способами: с помощью re и без.
+
+# Normal, task 2, version 1
+# Через re
+# Решение подсмотрел на чужом форке
+import re
 
 line_2 = 'mtMmEZUOmcqWiryMQhhTxqKdSTKCYEJlEZCsGAMkgAYEOmHBSQsSUHKvSfbmxULaysm'\
        'NOGIPHpEMujalpPLNzRWXfwHQqwksrFeipEUlTLeclMwAoktKlfUBJHPsnawvjPhfgewV'\
@@ -45,9 +74,37 @@ line_2 = 'mtMmEZUOmcqWiryMQhhTxqKdSTKCYEJlEZCsGAMkgAYEOmHBSQsSUHKvSfbmxULaysm'\
        'JFaXiUWgsKQrDOeZoNlZNRvHnLgCmysUeKnVJXPFIzvdDyleXylnKBfLCjLHntltignbQ'\
        'oiQzTYwZAiRwycdlHfyHNGmkNqSwXUrxGC'
 
+match = re.findall("[a-z]{2}([A-Z]+)[A-Z]{2}", line_2)
+print(match)
+
 # Задание-3:
 # Напишите скрипт, заполняющий указанный файл (самостоятельно задайте имя файла)
 # произвольными целыми цифрами, в результате в файле должно быть
 # 2500-значное произвольное число.
 # Найдите и выведите самую длинную последовательность одинаковых цифр
 # в вышезаполненном файле.
+
+# Normal, task 3
+# Смотрел чужой форк и гуглил, чтобы решить
+import random
+import os
+import itertools
+
+
+num_list = [random.randint(0, 9) for _ in range(2500)]
+number = int(''.join(str(i) for i in num_list))
+
+
+path = os.path.join('.', "number.txt")
+with open(path, 'w', encoding='UTF-8') as f:
+    f.write(str(number))
+
+with open(path, 'r', encoding='UTF-8') as f:
+    num_str = f.read()
+
+num_list = list(num_str)
+# В строке ниже до конца не разобрался. Поиск максимального значения понял, а
+# совместный синтаксис генератора и itertools нет.
+num_list_same_char = max((list(g) for k, g in itertools.groupby(num_list)), key=len)
+max_number = int(''.join(str(i) for i in num_list_same_char))
+print(max_number)
